a8704dd0d74722be7f8b0d57187b129d18cf0472
Johannes Cervantes <cervapz@gmail.com>
2020-06-17 22:30:34 -0600

    Se mejora el diseño con logos y iconos

--- UE_ManagerWebApp/Models/Tickets.cs
+++ UE_ManagerWebApp/Models/Tickets.cs
@@ -9,27 +9,40 @@ namespace UE_ManagerWebApp.Models
     public class Tickets
     {
         [Key]
+        [Display(Name = "ID")]
         public int Id { get; set; }
 
+        [Display(Name = "#Incidente")]
         public string TicketNumber { get; set; }
+
+        [Display(Name = "Palabra Clave")]
         public string Keyword { get; set; }
 
+        [Display(Name = "Fecha de  Asignación")]
         public DateTime? AssignDate { get; set; }
 
+        [Display(Name = "Fecha Final")]
         public DateTime? EndDate { get; set; }
 
+        [Display(Name = "Comentario")]
         public string Comment { get; set; }
 
+        [Display(Name = "Usuario de Creación")]
         public string CreationUser { get; set; }
 
+        [Display(Name = "Fecha de Creación")]
         public DateTime? CreationDate { get; set; }
 
+        [Display(Name = "Usuario de Creación")]
         public string UpdateUser { get; set; }
 
+        [Display(Name = "Fecha de Actualización")]
         public DateTime? UpdateDate { get; set; }
 
+        [Display(Name = "Fecha de Asignación")]
         public string AssignUser { get; set; }
 
+        [Display(Name = "Id Aplicación")]
         public int ApplicationId { get; set; }
         public Applications Application { get; set; }
 
