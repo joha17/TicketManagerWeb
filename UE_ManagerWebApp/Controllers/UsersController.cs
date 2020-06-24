10ebd6917b17b42c19f76995407b092c7fd01af3
Johannes <cervapz@gmail.com>
2020-06-23 18:07:52 -0600

    Se agrega mas validaciones de seguridad

--- UE_ManagerWebApp/Controllers/UsersController.cs
+++ UE_ManagerWebApp/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -79,6 +80,59 @@ namespace UE_ManagerWebApp.Controllers
                 throw;
             }
             
+        }
+
+
+        public async Task<IActionResult> ShowMyDataDetails()
+        {
+            try
+            {
+                string role;
+                if (TempData["UserRole"] != null)
+                    role = TempData["UserRole"] as string;
+
+                TempData.Keep();
+                
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+
+                // alternatively
+                // claimsIdentity = HttpContext.User.Identity as ClaimsIdentity;
+
+                // get some claim by type
+                //var someClaim = claimsIdentity.FindFirst("Username");
+                string username = claimsIdentity.FindFirst("Username").Value;
+                // iterate all claims
+                //foreach (var claim in claimsIdentity.Claims)
+                //{
+                //    if (claim.Type.Equals("Username"))
+                //    {
+                //        username = claim.Value;
+                //        if (username == null)
+                //        {
+                //            return NotFound();
+                //        }
+                //    }
+                //}
+                
+                if (username == null)
+                {
+                    return NotFound();
+                }
+                var users = await _context.Users
+                    .FirstOrDefaultAsync(m => m.Username == username);
+                if (users == null)
+                {
+                    return NotFound();
+                }
+
+                return View(users);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
 
         // GET: Users/Create
@@ -279,6 +333,96 @@ namespace UE_ManagerWebApp.Controllers
 
                 throw;
             }
+        }
+
+        public async Task<IActionResult> EditMyData(int? id)
+        {
+            try
+            {
+                string role;
+                if (TempData["UserRole"] != null)
+                    role = TempData["UserRole"] as string;
+
+                TempData.Keep();
+
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+                string username = claimsIdentity.FindFirst("Username").Value;
+
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var users = await _context.Users.FindAsync(id);
+                if (users == null)
+                {
+                    return NotFound();
+                }
+                if (users.Username.Equals(username))
+                {
+                    return View(users);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditMyData(int id, [Bind("Id,Username,Password,FirstName,LastName,EmailID,AccessLevel,ReadOnly")] Users users)
+        {
+            try
+            {
+                string role;
+                if (TempData["UserRole"] != null)
+                    role = TempData["UserRole"] as string;
+
+                TempData.Keep();
+
+                if (id != users.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        users.Password = MD5.MD5Crypto.Encrypt(users.Password);
+                        _context.Update(users);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!UsersExists(users.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(users);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
 
@@ -315,6 +459,7 @@ namespace UE_ManagerWebApp.Controllers
             }
             
         }
+
 
         // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
